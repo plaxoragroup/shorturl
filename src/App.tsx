f6c5338cccfac02c8b29f1326928cbc6ad91e1b5
Plaxora Group <infoplaxora@gmail.com>
2026-07-30 05:15:55 +0600

    Fix unused React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
